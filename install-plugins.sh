42574321cabd6001271cffbcfcf81c9eb9bac16d
Avshalom Toren <avshalomt2@gmail.com>
2021-09-06 00:24:55 +0300

    install-plugins: Use a simple list definition
    Use a unified list instead of 7 lines per plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
+# Add plugins to this list to install:
+plugins="wp-piwik contact-form-7 wordpress-seo ssl-insecure-content-fixer post-types-order flamingo wp-mail-smtp"
+
 COMMAND="sudo -u www-data wp --path=${WORDPRESS_PATH}"
 echo "Installing plugins and themes"
-
-#Append below for the plugin installation
 
 if [ $(${COMMAND} plugin is-installed hello) ]; then
   echo "Removing Useless Plugin hello"
   ${COMMAND} plugin delete hello
 fi
 
-if [ $(${COMMAND} plugin is-installed wp-piwik) ]; then
-  echo "Update piwik plugin for web analytics"
-  ${COMMAND} plugin update wp-piwik --activate
-else
-  echo "Install piwik plugin for web analytics"
-  ${COMMAND} plugin install wp-piwik --activate
-fi
-
-if [ $(${COMMAND} plugin is-installed twitter) ]; then
-  echo "Update twitter plugin for providing a social appearance to the site"
-  ${COMMAND} plugin update twitter --activate
-else
-  echo "Install twitter plugin for providing a social appearance to the site"
-  ${COMMAND} plugin install twitter --activate
-fi
-
-if [ $(${COMMAND} plugin is-installed cookie-notice) ]; then
-  echo "Update cookie notice plugin in order to sho information regarding cookies"
-  ${COMMAND} plugin update cookie-notice --activate
-else
-  echo "Install cookie notice plugin in order to sho information regarding cookies"
-  ${COMMAND} plugin install cookie-notice --activate
-fi
+for plugin in $plugins do
+  if [ $(${COMMAND} plugin is-installed ${plugin}) ]; then
+    echo "Updating plugin ${plugin}"
+    ${COMMAND} plugin update ${plugin} --activate
+  else
+    echo "Installing plugin ${plugin}"
+    ${COMMAND} plugin install ${plugin} --activate
+  fi
+done
